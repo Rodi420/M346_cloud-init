e07c70067b2d8a1a79c1c1b449c513037a787a5e
Rodi420 <105478013+Rodi420@users.noreply.github.com>
2022-12-07 08:50:30 +0100

    Update v1.1
    Version 1.1 07.12.2022
    
    - minor fixes

--- docker-install.sh
+++ docker-install.sh
@@ -6,11 +6,11 @@
 
 #start install docker packages
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli conteinerd.io docker-compose-plugin
-echo "installed successfully" >> /etc/var/could-init-output.log
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+echo "installed successfully" >> /etc/log/could-init-output.log
 
 #attempt docker test
-echo "attempting docker test" >> /etc/var/could-init-output.log
+echo "attempting docker test" >> /etc/log/could-init-output.log
 sudo docker run hello-world
 
 
