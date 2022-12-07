d7280818bafdc82b82d4ad0acc448efa9c27bef7
Rodi420 <105478013+Rodi420@users.noreply.github.com>
2022-12-07 08:57:29 +0100

    Update v1.2
    Version 1.2 07.12.2022
    
    - minor spelling issue
    - removed yes prompt for installation

--- docker-install.sh
+++ docker-install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # R.Tavares
-# 06.12.2022 v1.0
+# 06.12.2022 v1.2
 #
 #
 
 #start install docker packages
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-echo "installed successfully" >> /etc/log/could-init-output.log
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin --yes
+echo "installed successfully" >> /var/log/could-init-output.log
 
 #attempt docker test
-echo "attempting docker test" >> /etc/log/could-init-output.log
+echo "attempting docker test" >> /var/log/could-init-output.log
 sudo docker run hello-world
 
 
