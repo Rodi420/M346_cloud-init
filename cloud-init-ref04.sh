c6f7294710b4d87f45d4d270c1e1f9657754e1f7
Rodrigo <rodrigo.viamontes@outlook.com>
2022-12-21 11:50:21 +0100

    Update cloud-init-ref04
    fixed directory name

--- cloud-init-ref04.sh
+++ cloud-init-ref04.sh
@@ -12,7 +12,7 @@ echo "installed successfully" 2&>1 3&>1 1&>> /var/log/cloud-init-ref04-output.lo
 
 #make package and run it
 git clone https://github.com/Rodi420/M346_jar_files
-cd m346-ref-card-04-azure-main-edit
+cd M346_jar_files/m346-ref-card-04-azure-main-edit
 mvn package
 sleep 2
 mvn package
