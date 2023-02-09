a31a9551e04061c130684511778621209208cf7c
Rodi420 <rodrigo.viamontes@outlook.com>
2023-02-09 12:12:17 +0100

    Update v1.4
    Version 1.4
    
    docker-install:
    - added more logging
    
    cloud-init:
    - added same logging from docker
    - removed docker install code
    - added sudo permissions on all commands that require it

--- cloud-init.sh
+++ cloud-init.sh
@@ -1,34 +1,47 @@
 #!/bin/bash
 # R.Tavares
-# 07.12.2022 v1.1
+# 07.12.2022 v1.4
 #
 #
 
-#start install packages
+logfile="/var/log/docker_installer_rta.log"
+delimiter="----------------------------------------------"
+
+echo "$delimiter" >> $logfile
+echo "install packages" >> $logfile
+echo "$delimiter" >> $logfile
 sudo apt-get update
-sudo apt-get install openjdk-18-jdk maven ca-certificates curl gnupg lsb-release --yes
-echo "installed successfully" >> /var/log/could-init-output.log
+echo "$delimiter" >> $logfile
+sudo apt-get install openjdk-18-jdk maven ca-certificates curl gnupg lsb-release --yes >> $logfile
+echo "$delimiter" >> $logfile
 
 #prepare docker
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+#sudo mkdir -p /etc/apt/keyrings
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+#echo \
+#  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+#  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+#sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 #start install docker packages
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin --yes
-echo "installed successfully" >> /var/log/could-init-output.log
+#sudo apt-get update
+#sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin --yes
+#echo "installed successfully" >> /var/log/could-init-output.log
 
 #attempt docker test
-echo "attempting docker test" >> /var/log/could-init-output.log
-sudo docker run hello-world
+#echo "attempting docker test" >> /var/log/could-init-output.log
+#sudo docker run hello-world
 
-#make package and run it
-git clone https://gitlab.com/bbwrl/m346-ref-card-03.git
+#run package
+echo "make package and run it" >> $logfile
+echo "$delimiter" >> $logfile
+git clone https://gitlab.com/bbwrl/m346-ref-card-03.git >> $logfile
+echo "$delimiter" >> $logfile
 cd m346-ref-card-03
-sudo docker compose up --detach
-mvn package
-java -DDB_USERNAME='jokedbuser' -DDB_PASSWORD='123456' -jar target/architecture-refcard-03-0.0.1-SNAPSHOT.jar &
+echo "$delimiter" >> $logfile
+sudo docker compose up --detach >> $logfile
+echo "$delimiter" >> $logfile
+sudo mvn package >> $logfile
+echo "$delimiter" >> $logfile
+sudo java -DDB_USERNAME='jokedbuser' -DDB_PASSWORD='123456' -jar target/architecture-refcard-03-0.0.1-SNAPSHOT.jar & >> $logfile
+echo "$delimiter" >> $logfile
--- docker-install.sh
+++ docker-install.sh
@@ -1,33 +1,39 @@
 #!/bin/bash
 # R.Tavares
-# 06.12.2022 v1.3
+# 06.12.2022 v1.4
 # updated script for M169
 #
 
 logfile="/var/log/docker_installer_rta.log"
 delimiter="----------------------------------------------"
 
+#initial updates
+echo "$delimiter" >> $logfile
+echo "update packages" >> $logfile
 echo "$delimiter" >> $logfile
 sudo apt-get update >>$logfile
 echo "$delimiter" >> $logfile
 sudo apt-get install ca-certificates curl gnupg lsb-release --yes >> $logfile
+echo "$delimiter" >> $logfile
 
 #prepare docker
 sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-
-#start install docker packages
+#install docker packages
+echo "start install docker packages" >>$logfile
 echo "$delimiter" >> $logfile
 sudo apt-get update >> $logfile
 echo "$delimiter" >> $logfile
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin --yes >> $logfile
+echo "$delimiter" >> $logfile
 
-#attempt docker test
+#test attempt
+echo "attempt docker test" >> $logfile
 echo "$delimiter" >> $logfile
 sudo docker run hello-world >> $logfile
 echo "$delimiter" >> $logfile
