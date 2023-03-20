4fee4ca6aa78c277e3c22b57588f066ef6b63429
Rodi420 <rodrigo.viamontes@outlook.com>
2023-03-20 12:03:24 +0100

    Update v1.7.1
    Version 1.7.1
    
    docker&mysql-install:
    - small adjustments to logging irrelevant info instead of showing on terminal
    - small bug fixes
    - added more comments
    
    docker-install:
    - small bug fixes
    - added more comments

--- docker/docker&mysql-install.sh
+++ docker/docker&mysql-install.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 # R.Tavares
-# 16.03.2023 v1.7
+# 16.03.2023 v1.7.1
 # updated script for M169
 #
+# will probably merge this with base install script
+# and ask if user wants to install mysql etc.
 
-
-
-
+#variables
+  #EDITABLE
 logfile="/var/log/docker_installer_rta.log"
 delimiter="----------------------------------------------"
+  #DO NOT EDIT
 runtimeStart=$(date +%s%N)
 errorCode=0
+
 #check for sudo privileges 
 if [ "$EUID" -ne 0 ]; then
   errorCode=1
@@ -19,7 +22,10 @@ if [ "$EUID" -ne 0 ]; then
 fi
 echo -e "$delimiter\npermissions found. resume script...\n$delimiter" >> $logfile
 
+#print variables to log
 echo -e "logfile=$logfile\ndelimiter=$delimiter\nruntimeStart=$runtimeStart" >> $logfile
+
+#clear the screen
 clear
 
 #displayed on screen and log
@@ -29,7 +35,7 @@ echo -e "$delimiter\nPlease wait...\n$delimiter" 2>&1 | tee -a $logfile
 #initial updates
 echo "update packages" >> $logfile
 echo "$delimiter" >> $logfile
-sudo apt-get update >>$logfile
+sudo apt-get update >> $logfile
 echo "$delimiter" >> $logfile
 sudo apt-get install ca-certificates curl gnupg lsb-release --yes >> $logfile
 echo "$delimiter" >> $logfile
@@ -50,43 +56,49 @@ echo "$delimiter" >> $logfile
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin --yes >> $logfile
 echo "$delimiter" >> $logfile
 
-#test docker attempt
+#test docker functionality
 echo "attempt docker test" >> $logfile
 echo "$delimiter" >> $logfile
 sudo docker run hello-world >> $logfile 2>&1
 echo -e "$delimiter\nDocker Installed.\n$delimiter" 2>&1 | tee -a $logfile
 
-#start install mysql phpmyadmin.
+#start installing mysql & phpmyadmin.
 echo -e "$delimiter\nStarting MySQL & PhpMyAdmin.\n$delimiter" 2>&1 | tee -a $logfile
 
-#if dos2unix not found install it!!
+#if dos2unix not found install it
 package="dos2unix"
-
 if dpkg -s $package >/dev/null 2>&1
 then
   echo -e "$delimiter\ndos2unix is already installed. Resuming...\n$delimiter" 2>&1 | tee -a $logfile
 else
   echo -e "$delimiter\ndos2unix was not found. Installing...\n$delimiter" 2>&1 | tee -a $logfile
-  sudo apt-get update
-  sudo apt-get install -y $package
+  sudo apt-get update >> $logfile
+  echo "$delimiter" >> $logfile
+  sudo apt-get install -y $package >> $logfile
+  echo "$delimiter" >> $logfile
 fi
 
 #convert file or it wont work
 dos2unix docker-compose.yml 2>&1 | tee -a $logfile
+echo "$delimiter" >> $logfile
 
 #start mysql and phpmyadmin
 sudo docker compose up --detach >> $logfile 2>&1
 
 #displayed on screen and log
+  #script end + display active containers
 echo -e "$delimiter\nScript finished.\n$delimiter\nSee Log at: '$logfile'\n$delimiter\nSee running containers:\n$delimiter" 2>&1 | tee -a $logfile
 sudo docker ps -a 2>&1 | tee -a $logfile
 
+#math stuff idk
 runtimeEnd=$(date +%s%N)
 runtimeDiff=$(($runtimeEnd - $runtimeStart))
 runtimeDiffSec=$(
 		awk "BEGIN { print ($runtimeDiff/1000000000) }"
 	)
 echo -e "runtimeEnd=$runtimeEnd\n$delimiter" >> $logfile
+
 #displayed on screen and log
+  #display elapsed time
 echo -e "$delimiter\n${runtimeDiffSec}s elapsed.\n$delimiter" 2>&1 | tee -a $logfile
 
--- docker/docker-install.sh
+++ docker/docker-install.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 # R.Tavares
-# 06.12.2022 v1.5.2
+# 06.12.2022 v1.7.1
 # updated script for M169
 #
 
+#variables
+  #EDITABLE
 logfile="/var/log/docker_installer_rta.log"
 delimiter="----------------------------------------------"
+  #DO NOT EDIT
 runtimeStart=$(date +%s%N)
 errorCode=0
+
 #check for sudo privileges 
 if [ "$EUID" -ne 0 ]; then
   errorCode=1
@@ -16,19 +20,19 @@ if [ "$EUID" -ne 0 ]; then
 fi
 echo -e "$delimiter\npermissions found. resume script...\n$delimiter" >> $logfile
 
+#print variables to log
 echo -e "logfile=$logfile\ndelimiter=$delimiter\nruntimeStart=$runtimeStart" >> $logfile
+
+#clear the screen
 clear
 
 #displayed on screen and log
 echo -e "$delimiter\nPlease wait...\n$delimiter" 2>&1 | tee -a $logfile
 
-
 #initial updates
-
-
 echo "update packages" >> $logfile
 echo "$delimiter" >> $logfile
-sudo apt-get update >>$logfile
+sudo apt-get update >> $logfile
 echo "$delimiter" >> $logfile
 sudo apt-get install ca-certificates curl gnupg lsb-release --yes >> $logfile
 echo "$delimiter" >> $logfile
@@ -49,22 +53,23 @@ echo "$delimiter" >> $logfile
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin --yes >> $logfile
 echo "$delimiter" >> $logfile
 
-#test attempt
+#test docker functionality
 echo "attempt docker test" >> $logfile
 echo "$delimiter" >> $logfile
 sudo docker run hello-world >> $logfile 2>&1
 
-
 #displayed on screen and log
+  #script end 
 echo -e "$delimiter\nScript finished.\n$delimiter\nSee Log at: '$logfile'\n$delimiter" 2>&1 | tee -a $logfile
 
-
+#math stuff idk
 runtimeEnd=$(date +%s%N)
 runtimeDiff=$(($runtimeEnd - $runtimeStart))
 runtimeDiffSec=$(
 		awk "BEGIN { print ($runtimeDiff/1000000000) }"
 	)
 echo -e "runtimeEnd=$runtimeEnd\n$delimiter" >> $logfile
-#displayed on screen and log
-echo -e "${runtimeDiffSec}s elapsed.\n$delimiter" 2>&1 | tee -a $logfile
 
+#displayed on screen and log
+  #display elapsed time
+echo -e "$delimiter\n${runtimeDiffSec}s elapsed.\n$delimiter" 2>&1 | tee -a $logfile
