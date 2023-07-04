0e9ae18ff82c5b3ee702e35d9e91dffbc2a9f02c
Rodi420 <rodrigo.viamontes@outlook.com>
2023-07-04 16:43:55 +0200

    Update v1.11.6
    Version 1.11.6
    
    docker/nextcloud-mysql/auto-start.sh:
    - adjusted all variable names
    - moved variables to start where applicable
    - added function to only start 1 container
    - added function to use custom names on containers
    - added check on docker run if single container mode is on
    - added end message to say where nc is located

--- docker/nextcloud-mysql/dockerfile/auto-start.sh
+++ docker/nextcloud-mysql/dockerfile/auto-start.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 # R. Tavares
-# 28.06.2023 v1.11.5
+# 28.06.2023 v1.11.6
 # 
 #
 ###########################################
 # EDITABLE                                #
 ###########################################
-nextcloud_image=imageName1:latest
-mariadb_image=imageName2:latest
-custom_dir=yourDir
+nc_image=imageName1:latest
+db_image=imageName2:latest
+dir_name=yourDir
 ###########################################
 
-enable_build=0
+
+#used in en_conSingle
+nc_on=1
+db_on=1
+
+#used in en_conName
+nc_conName=nextcloud
+db_conName=mariadb
+
 
 #check for sudo privileges
 if [ "$EUID" -ne 0 ]; then
@@ -21,72 +29,125 @@ if [ "$EUID" -ne 0 ]; then
     exit
 fi
 
+#build images?
+en_build=0
+
 echo "Would you like to build images or just run the containers?"
 echo "1. Build and Run"
 echo "2. Run only (Default)"
-read -r -p "Input: " enable_build ;
+read -r -p "Input: " en_build ;
 
-if [[ $enable_build -eq 1 ]]
+#build images yes
+if [[ $en_build -eq 1 ]]
 then
     echo "What image name would you like to use for Nextcloud?"
-    read -r -p "Input: " nextcloud_name ;
+    read -r -p "Input: " nc_name ;
 
     echo "What would you like to name this Nextcloud version?"
-    read -r -p "Input: " nextcloud_version ;
+    read -r -p "Input: " nc_version ;
 
-    docker build -t "$nextcloud_name:$nextcloud_version" ./nc
+    docker build -t "$nc_name:$nc_version" ./nc
 
     echo "What image name would you like to use for MariaDB?"
-    read -r -p "Input: " mariadb_name ;
+    read -r -p "Input: " db_name ;
 
     echo "What would you like to name this MariaDB version?"
-    read -r -p "Input: " mariadb_version ;
+    read -r -p "Input: " db_version ;
 
-    docker build -t "$mariadb_name:$mariadb_version" ./db
+    docker build -t "$db_name:$db_version" ./db
 
-    nextcloud_image="$nextcloud_name:$nextcloud_version"
-    mariadb_image="$mariadb_name:$mariadb_version"
+    nc_image="$nc_name:$nc_version"
+    db_image="$db_name:$db_version"
 
+    #set custom directory name
     echo "Do you want to set a custom directory for your Container Volumes?"
-    echo "Default folder is: /$custom_dir/*"
+    echo "Default folder is: /$dir_name/*"
     echo "1. Yes"
     echo "2. No"
-    read -r -p "Input: " enable_customDir ;
+    read -r -p "Input: " en_dirName ;
 
-    if [[ $enable_customDir -eq 1 ]]
+    if [[ $en_dirName -eq 1 ]]
     then
         echo "What would you like to name your custom directory?"
-        read -r -p "Input: " custom_dir ;
+        read -r -p "Input: " dir_name ;
     fi
 fi
 
 #if any folders missing create them
-if [[ ! -d "/${custom_dir}" ]]
+if [[ ! -d "/${dir_name}" ]]
 then
     echo "Info: Custom Directory does not exist. Creating it..."
-    mkdir "/${custom_dir}"
+    mkdir "/${dir_name}"
 fi
-if [[ ! -d "/${custom_dir}/mariadb" ]]
+if [[ ! -d "/${dir_name}/mariadb" ]]
 then
     echo "Info: MariaDB Directory does not exist. Creating it..."
-    mkdir "/${custom_dir}/mariadb"
+    mkdir "/${dir_name}/mariadb"
 fi
-if [[ ! -d "/${custom_dir}/nextcloud-root" ]]
+if [[ ! -d "/${dir_name}/nextcloud-root" ]]
 then
     echo "Info: Nextcloud Root Directory does not exist. Creating it..."
-    mkdir "/${custom_dir}/nextcloud-root"
+    mkdir "/${dir_name}/nextcloud-root"
 fi
-if [[ ! -d "/${custom_dir}/nextcloud-data" ]]
+if [[ ! -d "/${dir_name}/nextcloud-data" ]]
 then
     echo "Info: Nextcloud Data Directory does not exist. Creating it..."
-    mkdir "/${custom_dir}/nextcloud-data"
+    mkdir "/${dir_name}/nextcloud-data"
 fi
-if [[ ! -d "/${custom_dir}/nextcloud-config" ]]
+if [[ ! -d "/${dir_name}/nextcloud-config" ]]
 then
     echo "Info: Nextcloud Config Directory does not exist. Creating it..."
-    mkdir "/${custom_dir}/nextcloud-config"
+    mkdir "/${dir_name}/nextcloud-config"
 fi
 
-# run each container
-docker run -d --name "mariadb" --volume /$custom_dir/mariadb:/var/lib/mysql $mariadb_image
-docker run -d --name "nextcloud" -p 8080:80 --link mariadb:mariadb --volume /$custom_dir/nextcloud-root:/var/www/html --volume /$custom_dir/nextcloud-data:/var/www/html/data --volume /$custom_dir/nextcloud-config:/var/www/html/config $nextcloud_image
+#run only 1 of the containers?
+en_conSingle=0
+
+echo "Do you want to start a single container or both?"
+echo "1. Single"
+echo "2. Both (Default)"
+read -r -p "Input: " en_conSingle ;
+
+if [[ $en_conSingle -eq 1 ]]
+then
+    echo "Which container would you like to start?"
+    echo "1. MariaDB"
+    echo "2. Nextcloud (Default)"
+    read -r -p "Input: "  en_conSingleChoice ;
+    if [[ $en_conSingleChoice -eq 1 ]]
+    then
+        nc_on=0
+    else
+        db_on=0
+    fi
+fi
+
+#run default or custom names
+en_conName=0
+
+echo "Do you want to use custom container names?"
+echo "Standard names are: '$nc_conName' and '$db_conName'"
+echo "1. Custom"
+echo "2. Standard (Default)"
+read -r -p "Input: " en_conName ;
+
+if [[ $en_conName -eq 1 ]]
+then
+    echo "What would you like to name your custom container for NEXTCLOUD?"
+    read -r -p "Input: " nc_conName ;
+    echo "What would you like to name your custom container for MARIADB?"
+    read -r -p "Input: " db_conName ;    
+fi
+
+
+#run each container
+if [[ $db_on -eq 1 ]]
+then
+    docker run -d --name "$db_conName" --volume /$dir_name/mariadb:/var/lib/mysql $db_image
+fi
+if [[ $nc_on -eq 1 ]]
+then
+    docker run -d --name "$nc_conName" -p 8080:80 --link "$db_conName":"$db_conName" --volume /$dir_name/nextcloud-root:/var/www/html --volume /$dir_name/nextcloud-data:/var/www/html/data --volume /$dir_name/nextcloud-config:/var/www/html/config $nc_image
+fi
+
+echo "Nextcloud '$nc_conName' using image '$nc_image' available at: http://localhost:8080"
