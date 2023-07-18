7d19d3d0001c76239950ed520b36d235b84cac97
Rodi420 <rodrigo.viamontes@outlook.com>
2023-07-18 10:46:26 +0200

    Update v1.12
    Version 1.12
    
    docker/php-sql-admin:
    - created
    - merged php-mysql and phpmyadmin-mariadb
    - adjusted some parameters
    - adjusted php scripts
    - adjusted compose

---
+++ docker/php-sql-admin/data/src/index.php
@@ -0,0 +1,40 @@
+<?php
+
+#credentials
+$host = 'mariadb';
+$user = 'rta-dbuser';
+$pass = '123456';
+$mydatabase = 'rta-db';
+
+#connection string
+$conn = new mysqli($host, $user, $pass, $mydatabase);
+
+#query
+$sql = 'SELECT * FROM user';
+
+#checks if connection is established
+if ($conn->connect_error) {
+        die("connection failed: " . $conn->connect_error);
+} else {
+        echo "connected to mysql";
+        echo "<br>";
+}
+
+
+if ($result = $conn->query($sql)) {
+        while ($data = $result->fetch_object()) {
+                $users[] = $data;
+        }
+}
+
+#lists all entries in the table (must have entries in db "rta-db" inside table "user")
+foreach ($users as $user) {
+        echo "<br>";
+        echo "name, password";
+        echo "<br>";
+        echo $user->name . "," . $user->password;
+        echo "<br>";
+}
+
+
+?>
